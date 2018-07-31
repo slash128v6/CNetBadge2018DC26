980f1b56fde0f22c2818bcea037caeed9e0ef572
slash128v6 <jg@slash128.com>
2018-07-30 19:56:39 -0700

    Add files via upload

--- patterns.h
+++ patterns.h
@@ -104,10 +104,10 @@ void pulsar()
 
   for (byte x = 0; x < kMatrixWidth; x++) {
     for (byte y = 0; y < kMatrixHeight; y++) {
-      leds[XY(x, y)] = CHSV(cycleHue, 0, quadwave8(x * 256 + y * 256 + ledPos));
+      leds[XY(x, y)] = CHSV(cycleHue, 0, quadwave8(x * 256 + y * 256 + currentLed));
     }
   }
 
-  ledPos -= 4;
+  currentLed -= 4;
 
 }
